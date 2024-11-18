715e80133115f6b7a7ceab8f60e4e883c6d22f4a
haldomae <hal.domae@gmail.com>
2024-11-18 10:16:43 +0900

    Holderの中身を記述

--- app/src/main/java/com/hal_domae/kadai08_pi12/recyclerview/ViewHolder.kt
+++ app/src/main/java/com/hal_domae/kadai08_pi12/recyclerview/ViewHolder.kt
@@ -1,4 +1,12 @@
 package com.hal_domae.kadai08_pi12.recyclerview
 
-class ViewHolder {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hal_domae.kadai08_pi12.R
+
+// RecyclerViewで使いたいパーツを定義
+class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val itemDate: TextView = itemView.findViewById(R.id.date)
+    val itemText: TextView = itemView.findViewById(R.id.text)
 }
