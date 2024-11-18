a74f1bf087e12631e4e23b2dafac92bf5d54e45c
haldomae <hal.domae@gmail.com>
2024-11-18 10:46:05 +0900

    Adapterの中身を記述

--- app/src/main/java/com/hal_domae/kadai08_pi12/recyclerview/ListAdapter.kt
+++ app/src/main/java/com/hal_domae/kadai08_pi12/recyclerview/ListAdapter.kt
@@ -1,4 +1,24 @@
 package com.hal_domae.kadai08_pi12.recyclerview
 
-class ListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hal_domae.kadai08_pi12.R
+
+class ListAdapter(private val data: MutableList<Map<String, String>>): RecyclerView.Adapter<ViewHolder>() {
+    // ViewHolderを作成する
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
+    }
+
+    // 表示するデータの件数を調べる
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    // ViewHolderの中身を変更
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemDate.text = data[position]["date"]
+        holder.itemText.text = data[position]["text"]
+    }
 }
