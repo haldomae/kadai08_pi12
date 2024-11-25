94ff1b6ff2d2aa2c06094dddaea0f3c4b758a6b4
haldomae <hal.domae@gmail.com>
2024-11-25 10:08:08 +0900

    データベースからデータを取得する準備

--- app/src/main/java/com/hal_domae/kadai08_pi12/MainActivity.kt
+++ app/src/main/java/com/hal_domae/kadai08_pi12/MainActivity.kt
@@ -12,6 +12,7 @@ import com.hal_domae.kadai08_pi12.recyclerview.ListAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dbHelper: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +23,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // データベースから取り出したデータを入れる
+        val data = mutableListOf<Map<String, String>>()
+        // データベースを用意
+        dbHelper = DatabaseHelper(this@MainActivity)
+        // データベースからデータを取り出す
+        dbHelper.readableDatabase.use { db -> }
 
         val sampleData = mutableListOf(
             mapOf("date" to "2024/01/01", "text" to "ここに日記のテキストが入ります"),
