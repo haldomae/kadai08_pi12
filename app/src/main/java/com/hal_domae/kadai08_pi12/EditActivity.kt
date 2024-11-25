b3693e2ad801298e388d51336d411627dca1f0cb
haldomae <hal.domae@gmail.com>
2024-11-25 11:45:02 +0900

    EditActivityのviewbindingの設定を有効にする

--- app/src/main/java/com/hal_domae/kadai08_pi12/EditActivity.kt
+++ app/src/main/java/com/hal_domae/kadai08_pi12/EditActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hal_domae.kadai08_pi12.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
