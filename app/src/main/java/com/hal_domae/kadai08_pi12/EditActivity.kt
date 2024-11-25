6602269da86523e6ea0d9980e6193f0d03c138d2
haldomae <hal.domae@gmail.com>
2024-11-25 12:12:18 +0900

    気分のアイコンを押した時に文章を挿入

--- app/src/main/java/com/hal_domae/kadai08_pi12/EditActivity.kt
+++ app/src/main/java/com/hal_domae/kadai08_pi12/EditActivity.kt
@@ -1,6 +1,7 @@
 package com.hal_domae.kadai08_pi12
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,7 @@ import com.hal_domae.kadai08_pi12.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
+    private var textFeeling: String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,10 +21,29 @@ class EditActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // 気分を押した時のイベントリスナー
+        binding.feelGreat.setOnClickListener { feelClicked(it) }
+        binding.feelGood.setOnClickListener { feelClicked(it) }
+        binding.feelNormal.setOnClickListener { feelClicked(it) }
+        binding.feelBad.setOnClickListener { feelClicked(it) }
+        binding.feelAwful.setOnClickListener { feelClicked(it) }
 
         binding.selectDate.setOnClickListener {
             val datePicker = DatePickerFragment()
             datePicker.show(supportFragmentManager, "datePicker")
         }
+    }
+
+    // 気分が選択されたとき
+    private fun feelClicked(view: View){
+        textFeeling = when(view.id){
+            R.id.feel_great -> "今日の気分は最高でした！"
+            R.id.feel_good -> "今日の気分はいい！"
+            R.id.feel_normal -> "今日の気分は普通"
+            R.id.feel_bad -> "今日の気分は微妙"
+            else -> "今日の気分は最悪..."
+        }
+        // テキストを置き換える
+        binding.inputDiary.setText(getString(R.string.diary_text, textFeeling, ""))
     }
 }
